b94f7e71b85aaea2f83020ca9a358bc72205788b
Swonboyyash <parikhyash366@gmail.com>
2017-09-20 10:20:32 -0700

    Created a component for Navigtion Bar

--- harshasKitchen/src/app/app.module.ts
+++ harshasKitchen/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule} from "@angular/forms";
 
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
